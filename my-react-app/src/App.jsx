69cff0b4cba42d93a65516ff86024651315212c1
ranjananushkacsv <ranjananushka90@gmail.com>
2025-02-27 20:45:22 +0530

    routedd

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './screens/Navbar/Navbar';
-import Home from './screens/Home/Home';
-import Dashboard from './screens/Dashboard/Dashboard';
+import Navbar from './screen/Navbar/Navbar';
+import Home from './screen/Home/Home';
+import Dashboard from './screen/Dashboard/Dashboard';
 import Settings from './screen/Settings/Settings';
 import Profile from './screen/Profile/Profile';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar /> {/* Placing Navbar outside so it remains visible on all pages */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="dashboard" element={<Dashboard />}>
-          <Route path="profile" element={<Profile />} />
-          <Route path="settings" element={<Settings />} />
+        {}
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />}>
+            <Route path="profile" element={<Profile />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
