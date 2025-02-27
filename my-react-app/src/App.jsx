804c162bf8fb1ccba942483e4b16b3d195a956d9
ranjananushkacsv <ranjananushka90@gmail.com>
2025-02-27 21:22:18 +0530

    useeffect

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,25 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './screen/Navbar/Navbar';
-import Home from './screen/Home/Home';
-import Dashboard from './screen/Dashboard/Dashboard';
-import Settings from './screen/Settings/Settings';
-import Profile from './screen/Profile/Profile';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {}
-        <Route path="/" element={<Navbar />}>
-          <Route index element={<Home />} />
-          <Route path="dashboard" element={<Dashboard />}>
-            <Route path="profile" element={<Profile />} />
-            <Route path="settings" element={<Settings />} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(interval); 
+  }, []);
+
+  return <h1>{count}</h1>;
 }
 
 export default App;
